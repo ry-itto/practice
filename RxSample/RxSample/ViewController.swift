2a666d3b723552271ac052a9fe26c4c66b7e7eaa
ryoya_ito <horusu.dqx@gmail.com>
2019-03-11 23:31:32 +0900

    skip, take追加

--- RxSample/RxSample/ViewController.swift
+++ RxSample/RxSample/ViewController.swift
@@ -22,15 +22,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let pLabel = publish,
+        guard
+            let pLabel = publish,
             let bLabel = behavior
-            else {
-                return
+        else {
+            return
         }
         
-        // Publish(RxCocoaを使用してbindを利用した実装)
+        // Publish(RxCocoaを使用してbindを利用した実装, 3回しかイベントが流れ切らない)
         // bind内でpublishを利用しようとするとOptionalのため，nilの可能性がある関係で「Generic parameter 'Self' could not be inferred」が発生する
         _ = viewModel.publishObservable
+            .take(3)
             .map({"\($0)"})
             .bind(to: pLabel.rx.text)
             .disposed(by: disposeBag)
@@ -41,8 +43,10 @@ class ViewController: UIViewController {
             .bind(to: bLabel.rx.text)
             .disposed(by: disposeBag)
         
-        // Variable(Subscribeでの実装)
-        _ = viewModel.countObservable.subscribe(onNext: { [unowned self] count in
+        // Variable(Subscribeでの実装, 最初から2つのイベントは購読しない)
+        _ = viewModel.countObservable
+            .skip(2)
+            .subscribe(onNext: { [unowned self] count in
             DispatchQueue.main.async {
                 self.variable?.text = "\(count)"
             }
