a0327b3b33529faee6c4024ddde071c9ce5682fa
ryoya_ito <horusu.dqx@gmail.com>
2019-08-25 22:48:52 +0900

    プッシュ通知サンプル作成

---
+++ PushNotificationSample/PushNotificationSample/AppDelegate.swift
@@ -0,0 +1,66 @@
+//
+//  AppDelegate.swift
+//  PushNotificationSample
+//
+//  Created by 伊藤凌也 on 2019/08/25.
+//  Copyright © 2019 ry-itto. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+
+    var window: UIWindow?
+    let gcmMessageIDKey = "gcm.message_id"
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        self.window = window
+        self.window?.makeKeyAndVisible()
+        FirebaseApp.configure()
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions) { ok, error in
+                if ok {
+                    print("ok")
+                } else {
+                    print("fail")
+                }
+            }
+            
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Receive Registration Token: \(fcmToken)")
+    }
+}
---
+++ PushNotificationSample/PushNotificationSample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PushNotificationSample
+//
+//  Created by 伊藤凌也 on 2019/08/25.
+//  Copyright © 2019 ry-itto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
